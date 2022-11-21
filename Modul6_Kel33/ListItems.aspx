6446ebb3bb66e999bbdcc64130dcf8f38131683a
sukasulfat <rsudsukaadm@gmail.com>
2022-11-21 20:28:52 +0700

    Add project files.

---
+++ Modul6_Kel33/ListItems.aspx
@@ -0,0 +1,134 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ListItems.aspx.cs" Inherits="Modul6_Kel33.ListItems" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       <div class="row">
+          <div style="width: 40%; float: left;" class="left">
+              <h1>Form untuk mahasiswa</h1>
+              <table>
+                <tr>
+                  <td>NIM :</td>
+                  <td>
+                    <asp:TextBox ID="txtNimMhs" runat="server"></asp:TextBox>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Nama :</td>
+                  <td>
+                    <asp:TextBox ID="txtNamaMhs" runat="server"></asp:TextBox>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Alamat :</td>
+                  <td>
+                    <asp:TextBox ID="txtAlamatMhs" runat="server"></asp:TextBox>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Hobi :</td>
+                  <td>
+                    <asp:TextBox ID="txtHobiMhs" runat="server"></asp:TextBox>
+                  </td>
+                </tr>
+                <tr>
+                  <td>ID Jurusan :</td>
+                  <td>
+                    <asp:TextBox ID="txtIdJurusanMhs" runat="server"></asp:TextBox>
+                  </td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>
+                    <asp:Button
+                      ID="btnAddMhs"
+                      runat="server"
+                      Text="ADD"
+                      OnClick="btnAddMhs_Click"
+                    />
+                    <asp:Button
+                      ID="btnDeleteMhs"
+                      runat="server"
+                      Text="DELETE"
+                      OnClick="btnDeleteMhs_Click"
+                    />
+                    <asp:Button
+                      ID="btnUpdateMhs"
+                      runat="server"
+                      Text="UPDATE"
+                      OnClick="btnUpdateMhs_Click"
+                    />
+                   <asp:Button
+                      ID="btnClearMhs"
+                      runat="server"
+                      Text="CLEAR"
+                      OnClick="btnClearMhs_Click"
+                    />
+                  </td>
+                </tr>
+              </table>
+               <asp:GridView ID="GridViewMhs" runat="server">
+                </asp:GridView>
+        </div>
+        <div style="width: 50%; float: right;" class="right">
+            <h1>Form untuk jurusan</h1>
+              <table>
+                <tr>
+                  <td>ID Jurusan :</td>
+                  <td>
+                    <asp:TextBox ID="txtIdJurusan" runat="server"></asp:TextBox>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Nama Jurusan :</td>
+                  <td>
+                    <asp:TextBox ID="txtNamaJurusan" runat="server"></asp:TextBox>
+                  </td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>
+                    <asp:Button
+                      ID="btnAddJurusan"
+                      runat="server"
+                      Text="ADD"
+                      OnClick="btnAddJurusan_Click"
+                    />
+                    <asp:Button
+                      ID="btnDeleteJurusan"
+                      runat="server"
+                      Text="DELETE"
+                      OnClick="btnDeleteJurusan_Click"
+                    />
+                    <asp:Button
+                      ID="btnUpdateJurusan"
+                      runat="server"
+                      Text="UPDATE"
+                      OnClick="btnUpdateJurusan_Click"
+                    />
+                   <asp:Button
+                      ID="btnClearJurusan"
+                      runat="server"
+                      Text="CLEAR"
+                      OnClick="btnClearJurusan_Click"
+                    />
+                  </td>
+                </tr>
+              </table>
+               <asp:GridView ID="GridViewJurusan" runat="server">
+                </asp:GridView>
+        </div>
+        <div style="clear: both"></div>
+        </div>
+        <div style="margin: 20px">
+             <asp:GridView ID="GridViewJoin" runat="server">
+                </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Modul6_Kel33/ListItems.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+
+namespace Modul6_Kel33
+{
+    public partial class ListItems : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter sda = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = "Data Source=10.0.10.172;Initial Catalog=TugasMod6DB_Kel33;User ID=sa;Password=dc123";
+            con.Open();
+            if (!Page.IsPostBack)
+            {
+                refreshAllGridView();
+            }
+        }
+
+        public void refreshAllGridView()
+        {
+            DataShowMhs();
+            DataShowJurusan();
+            DataShowJoin();
+        }
+        public void DataShowMhs()
+        {
+            ds = new DataSet();
+            cmd.CommandText = "Select * from MAHASISWA";
+            cmd.Connection = con;
+            sda = new SqlDataAdapter(cmd);
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+
+            GridViewMhs.DataSource = ds;
+            GridViewMhs.DataBind();
+        }
+
+        public void DataShowJurusan()
+        {
+            ds = new DataSet();
+            cmd.CommandText = "Select * from JURUSAN";
+            cmd.Connection = con;
+            sda = new SqlDataAdapter(cmd);
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+
+            GridViewJurusan.DataSource = ds;
+            GridViewJurusan.DataBind();
+        }
+
+        public void DataShowJoin()
+        {
+            ds = new DataSet();
+            cmd.CommandText = "Select * from MAHASISWA_JURUSAN";
+            cmd.Connection = con;
+            sda = new SqlDataAdapter(cmd);
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+
+            GridViewJoin.DataSource = ds;
+            GridViewJoin.DataBind();
+            con.Close();
+        }
+
+        protected void btnAddMhs_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Insert Into MAHASISWA(ID_MAHASISWA, ID_JURUSAN, NAMA_MAHASISWA, ALAMAT_MAHASISWA, HOBI_MAHASISWA) values('" + txtNimMhs.Text + "', '" + txtIdJurusanMhs.Text.ToString() + "', '" + txtNamaMhs.Text.ToString() + "','" + txtAlamatMhs.Text.ToString() + "','" + txtHobiMhs.Text.ToString() + "')";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            refreshAllGridView();
+        }
+
+        protected void btnDeleteMhs_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Delete from MAHASISWA where ID_MAHASISWA='" + txtNimMhs.Text.ToString() + "'";
+            cmd.Connection = con; 
+            cmd.ExecuteNonQuery();
+            refreshAllGridView();
+        }
+
+        protected void btnUpdateMhs_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Update MAHASISWA set ID_JURUSAN='" + txtIdJurusanMhs.Text.ToString() + "', NAMA_MAHASISWA='" + txtNamaMhs.Text.ToString() + "',ALAMAT_MAHASISWA='" + txtAlamatMhs.Text.ToString() + "',HOBI_MAHASISWA='" + txtHobiMhs.Text.ToString() + "' WHERE ID_MAHASISWA='" + txtNimMhs.Text.ToString() + "'";
+
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            refreshAllGridView();
+        }
+
+        protected void btnClearMhs_Click(object sender, EventArgs e)
+        {
+            txtNimMhs.Text = null;
+            txtIdJurusan.Text = null;
+            txtNamaMhs.Text = null;
+            txtAlamatMhs.Text = null;
+            txtHobiMhs.Text = null;
+        }
+
+        protected void btnAddJurusan_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Insert Into JURUSAN(ID_JURUSAN, NAMA_JURUSAN) values('" + txtIdJurusan.Text + "', '" + txtNamaJurusan.Text.ToString() + "')";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            refreshAllGridView();
+        }
+
+        protected void btnDeleteJurusan_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Delete from JURUSAN where ID_JURUSAN='" + txtIdJurusan.Text.ToString() + "'";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            refreshAllGridView();
+        }
+
+        protected void btnUpdateJurusan_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Update JURUSAN set NAMA_JURUSAN='" + txtNamaJurusan.Text.ToString() + "' WHERE ID_JURUSAN='" + txtIdJurusan.Text.ToString() + "'";
+
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            refreshAllGridView();
+        }
+
+        protected void btnClearJurusan_Click(object sender, EventArgs e)
+        {
+            txtIdJurusan.Text = null;
+            txtNamaJurusan.Text = null;
+        }
+
+
+    }
+}
